d1ded6f78d6a559031961e823a2e64444c9a2a82
TrangTrang <quynhtrang11314@gmail.com>
2025-07-22 12:03:34 +0700

    OTP

--- Nhom7_DoAn_DangKy_DangNhap/Services/EmailService.cs
+++ Nhom7_DoAn_DangKy_DangNhap/Services/EmailService.cs
@@ -26,21 +26,23 @@ namespace Nhom7_DoAn_DangKy_DangNhap.Services
             using (var client = new SmtpClient(_settings.SmtpServer, _settings.Port))
             {
                 client.Credentials = new NetworkCredential(_settings.SenderEmail, _settings.SenderPassword);
-                client.EnableSsl = true;
-                client.UseDefaultCredentials = false;
+                client.EnableSsl = true;  // Gmail bắt buộc SSL
+                client.UseDefaultCredentials = false; // Bắt buộc false để dùng Credentials
 
                 var message = new MailMessage
                 {
-                    From = new MailAddress(_settings.SenderEmail),
+                    From = new MailAddress(_settings.SenderEmail, "Hệ thống đặt vé"), // Có thể thêm tên gửi
                     Subject = subject,
                     Body = body,
                     IsBodyHtml = true
                 };
 
-                message.To.Add(toEmail);
+                message.To.Add(new MailAddress(toEmail));
 
                 await client.SendMailAsync(message);
             }
         }
+
     }
 }
+
